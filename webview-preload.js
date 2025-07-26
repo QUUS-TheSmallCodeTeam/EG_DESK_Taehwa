1bb6205489837aca19f29330b9a1e0c291aa6263
MinsooCha <m8chaa@gmail.com>
2025-07-26 17:40:30 +0900

    unnecessary files deletion

--- webview-preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-    notifyParent: (message) => ipcRenderer.send('webview-message', message),
-    getURL: () => ipcRenderer.invoke('get-webview-url'),
-    canGoBack: () => ipcRenderer.invoke('can-go-back'),
-    canGoForward: () => ipcRenderer.invoke('can-go-forward'),
-    validateURL: (url) => ipcRenderer.invoke('validate-url', url)
-});
